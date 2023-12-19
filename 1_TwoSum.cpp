bd2abb307b9689693572fde98f05441cb67642bb
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2023-12-19 07:50:18 +0530

    Update 1_TwoSum.cpp

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -1,1 +1,19 @@
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans; // Creating answer vector
+        unordered_map<int, int> m; // Creating an unordered map that will store the freqency of any element
+        for (int i = 0; i < nums.size(); i++) {
+          // check wheater the element is present or not
+          // If present then will return the answer
+            if (m.find(target - nums[i])  != m.end()) {
+                ans.push_back(m[target - nums[i]]);
+                ans.push_back(i);
+                break;
+            }
+            m[nums[i]] = i; // Not present then increase the freaquency of the curr element and move to the next element
+        }
+        return ans; // Return ans 
+    }
+};
