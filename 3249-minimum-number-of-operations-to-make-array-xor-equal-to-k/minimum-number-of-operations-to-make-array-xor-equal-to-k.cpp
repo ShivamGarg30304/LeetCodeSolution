04088a9efa7cc6c840d937a8050992748b62272a
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2024-04-29 08:26:27 +0530

    Time: 342 ms (5.06%) | Memory: 95.2 MB (6.81%) - LeetSync

---
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        map<int, int> mp;
+        map<int, int> mp2;
+        map<int, int> mp3;
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < n; j++) {
+                int newnum = 1 << i;
+                if (nums[j] & newnum) {
+                    mp[i]++;
+                }
+            }
+        }
+        for (int i = 0; i < 30; i++) {
+            int newnum = 1 << i;
+            if (k & newnum) {
+                mp2[i]++;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < 30; i++) {
+            int num = i;
+            int x = mp[i];
+            int u, v;
+            if (x % 2 == 0) u = 0;
+            else u = 1;
+            v = 0;
+            if (mp2[num] == 1) {
+                if (u == v) ans++;
+            } else {
+                if (u != v) ans++;
+            }
+        }
+        return ans;
+    }
+};
