d528b7443a9253296ebc59745d12a39218e051e2
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2024-07-09 22:34:52 +0530

    Time: 3 ms (83.42%) | Memory: 13.4 MB (97.68%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    void kDown(TreeNode* root, int k, TreeNode* blockNode, vector<int> &ans) {
+        if (root == NULL || k < 0 || root == blockNode) return;
+        if (k == 0) {
+            ans.push_back(root -> val);
+            return;
+        }
+        kDown(root -> left, k - 1, blockNode, ans);
+        kDown(root -> right, k - 1, blockNode, ans);
+    }
+    int solve(TreeNode* root, TreeNode* target, int k, vector<int> &ans) {
+        if (root == NULL) return -1;
+        if (root == target) {
+            kDown(root, k - 0, NULL, ans);
+            return 1;
+        }
+        int left = solve(root -> left, target, k, ans);
+        if (left != -1) {
+            kDown(root, k - left, root -> left, ans);
+            return left + 1;
+        }
+        int right = solve(root -> right, target, k, ans);
+        if (right != -1) {
+            kDown(root, k - right, root -> right, ans);
+            return right + 1;
+        }
+        return -1;
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> ans;
+        solve(root, target, k, ans);
+        return ans;
+    }
+};
