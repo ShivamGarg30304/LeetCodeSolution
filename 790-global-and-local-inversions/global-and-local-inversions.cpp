48482b15bdef0b4eebb5bf2cc7a186984dc4945e
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2024-07-19 23:47:55 +0530

    Time: 110 ms (42.93%) | Memory: 86 MB (40.25%) - LeetSync

--- 790-global-and-local-inversions/global-and-local-inversions.cpp
+++ 790-global-and-local-inversions/global-and-local-inversions.cpp
@@ -1,55 +1,13 @@
 class Solution {
-    long long ans = 0;
-    void merge(vector<int> &nums, int low, int mid, int high) {
-        int n1 = mid - low + 1;
-        int n2 = high - mid;
-        vector<int> left(n1), right(n2);
-        for (int i = 0; i < n1; i++) {
-            left[i] = nums[low + i];
-        }
-        for (int i = 0; i < n2; i++) {
-            right[i] = nums[mid + i + 1];
-        }
-        int i = 0, j = 0, k = low;
-        while (i < n1 && j < n2) {
-            if (left[i] <= right[j]) {
-                nums[k] = left[i];
-                i++;
-            } else {
-                ans += (n1 - i);
-                nums[k] = right[j];
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            nums[k] = left[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            nums[k] = right[j];
-            j++;
-            k++;
-        }
-    }
-    void mergeSort(vector<int> &nums, int low, int high) {
-        if (low < high) {
-            int mid = (low + high) / 2;
-            mergeSort(nums, low, mid);
-            mergeSort(nums, mid + 1, high);
-            merge(nums, low, mid, high);
-        }
-    }
 public:
     bool isIdealPermutation(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                ans = ans - 1;
+        int i = 0;
+        while (i < nums.size()) {
+            if (abs(i - nums[i]) > 1) {
+                return false;
             }
+            i++;
         }
-        mergeSort(nums, 0, n - 1);
-        return (ans == 0);
+        return true;
     }
 };
