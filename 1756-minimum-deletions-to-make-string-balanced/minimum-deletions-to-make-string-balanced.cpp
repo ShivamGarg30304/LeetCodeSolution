405faca7e09a4c597518606a810e362c18e5a786
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2024-07-31 00:28:13 +0530

    Time: 70 ms (96.17%) | Memory: 23.6 MB (73.37%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        vector<int> count_a(n, 0);
-        int a_count = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            count_a[i] = a_count;
-            if (s[i] == 'a') a_count++;
+        int n = s.length(), a_count = 0;
+        for (int i = 0; i < n; i++) {
+            a_count += (s[i] == 'a');
         }
         int b_count = 0;
         int min_deletions = n;
         for (int i = 0; i < n; i++) {
-            min_deletions = min(min_deletions, count_a[i] + b_count);
+            a_count -= (s[i] == 'a');
+            min_deletions = min(min_deletions, a_count + b_count);
             b_count += (s[i] == 'b');
         }
 
