2c5c7317fb6e27389f6863b3ab0c9d3add24bdb8
ShivamGarg30304 <99480259+ShivamGarg30304@users.noreply.github.com>
2024-07-31 12:33:59 +0530

    Time: 61 ms (43.19%) | Memory: 53.8 MB (46.96%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -7,18 +7,12 @@ public:
                 nums[i] = n + 1;
             }
         }
-        for (auto x : nums) {
-            cout << x << " ";
-        }
-        cout << endl;
         for (int i = 0; i < n; i++) {
             if (abs(nums[i]) == n + 1) continue;
             int value = abs(nums[i]) - 1;
-            cout << value << endl;
             if (nums[value] > 0) nums[value] = -nums[value];
         }
         for (int i = 0; i < n; i++) {
-            // if (nums[i] == n + 1) continue;
             if (nums[i] > 0) return i + 1;
         }
         return n + 1;
